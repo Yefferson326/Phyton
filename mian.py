b84569a29e30952c23e263140859e0f6366d8efb
Yefferson326 <89107160+Yefferson326@users.noreply.github.com>
2021-11-09 10:50:30 -0500

    Print in Phyton

---
+++ mian.py
@@ -0,0 +1,13 @@
+
+string_var = "Hola clase de electiva"
+integer_var = 123
+float_var = 123.24
+
+print(string_var)
+print(type(string_var))
+
+print(integer_var)
+print(type(integer_var))
+
+print(float_var)
+print(type(float_var))
