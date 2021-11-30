16de0168d268a58fb748f467b27ceebe13d301fc
Yefferson Arley Nuñez Camacho <yefferson.nunez@uptc.edu.co>
2021-11-30 10:40:11 -0500

    Introduccion de funciones para pedir los numeros

--- CALCULADORA/Main.py
+++ CALCULADORA/Main.py
@@ -6,37 +6,41 @@ print("|  |      |  | |  ||  |     |  |     |  |  |  | |  |     |  | |  | | | / 
 print("|  |____  |  | |  ||  |____ |  |____ |  |__|  | |  |____ |  | |  | | |/ / |       | |  |\ \ |  | |  |")
 print("|_______| |__| |__||_______||_______||________| |_______||__| |__| |___/  |_______| |__| |_\|__| |__|")
 
+def getNumOne ():
+    print("Ingrese el primer número")
+    num1 = float(input("-->"))
+    return  num1
+def getNumTwo():
+    print("Ingrese el segundo número")
+    num2 = float(input("-->"))
+    return num2
 
 print("\t\t#####Es este programa podras realizar operaciones basicas para dos numeros#####")
 repeat = 1
+num1= float()
+num2= float()
 while (repeat == 1):
     print("¿Que operacion desea realizar?")
     print("1.SUMA\n2.RESTA\n3.MULTIPLICACION\n4.DIVISION")
     operation= int(input("-->"))
     if operation == 1:
         print("\t\t##SUMA##")
-        print("Ingrese el primer número")
-        num1 = float(input("-->"))
-        print("Ingrese el segundo número")
-        num2 = float(input("-->"))
+        num1= getNumOne()
+        num2= getNumTwo()
         print("\t",num1," + ",num2," = ",num1+num2)
         print("Desea realizar otra operacion\n1.SI\n2.NO")
         repeat = int(input("-->"))
     elif operation == 2:
         print("\t\t##RESTA##")
-        print("Ingrese el primer número")
-        num1 = float(input("-->"))
-        print("Ingrese el segundo número")
-        num2 = float(input("-->"))
+        num1= getNumOne()
+        num2= getNumTwo()
         print("\t",num1, " - ", num2, " = ", num1 - num2)
         print("Desea realizar otra operacion\n1.SI\n2.NO")
         repeat = int(input("-->"))
     elif operation == 3:
         print("\t\t##MULTIPLICACION##")
-        print("Ingrese el primer número")
-        num1 = float(input("-->"))
-        print("Ingrese el segundo número")
-        num2 = float(input("-->"))
+        num1 = getNumOne()
+        num2 = getNumTwo()
         print("\t",num1, " x ", num2, " = ", num1 * num2)
         print("Desea realizar otra operacion\n1.SI\n2.NO")
         repeat = int(input("-->"))
@@ -44,10 +48,8 @@ while (repeat == 1):
         checkDivisor = 1
         while checkDivisor == 1:
             print("\t\t##DIVISION##")
-            print("Ingrese el dividendo")
-            num1 = float(input("-->"))
-            print("Ingrese el divisor")
-            num2 = float(input("-->"))
+            num1 = getNumOne()
+            num2 = getNumTwo()
             if num2 == 0:
                 print("\t¡¡El resultado de ",num1," entre ",num2," da un valor indefinido¡¡\n\t<<Por favor ingrese un valor para el divisor diferente de 0>>\n")
             else:
