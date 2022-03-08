8c41e55c38a93b140b33854bf1dbe2c7a8c1d6d5
Yefferson Arley Nuñez Camacho <yefferson.nunez@uptc.edu.co>
2022-03-08 10:29:12 -0500

    Se mandaron algunas funciones a otro archivo, de tal forma que el codigo central del programa se viera un poco mas pequeño y mas facil de leer, dichas funciones son compartidas tanto para la agenda utilzando diccionarios, como la agenda usando clases

--- AGENDA DE CONTACTOS/Agenda_Diccionarios.py
+++ AGENDA DE CONTACTOS/Agenda_Diccionarios.py
@@ -1,4 +1,6 @@
+from Agenda_con_clases import funciones_generales
 Contacts = []
+
 
 def view_menu():
     consulting = True
@@ -19,28 +21,10 @@ def view_menu():
                 "1": check_contact,
                 "2": look_for_contact,
                 "3": add_contact,
-                "4": exit
+                "4": funciones_generales.exit
             }
-            operation = menu.get(option, error)
+            operation = menu.get(option, funciones_generales.error)
             consulting = operation()
-
-
-def error():
-    print("\U0000274C\U0000274C\U0000274COPCION INCORRECTA\U0000274C\U0000274C\U0000274C\n\t Intenta de nuevo")
-
-
-def check_continue():
-    option = 0
-    while option != (1, 2):
-        print("¿Desea regresar al menu?\n1.SI\n2.NO")
-        option = int(input("-->"))
-        if option == 1:
-            return True
-        elif option == 2:
-            exit()
-            return False
-        else:
-            error()
 
 
 def check_contact():
@@ -57,59 +41,14 @@ def check_contact():
             print("APELLIDO:", person["Apellido"])
             print("TELEFONO:", person["Telefono"])
             print("CEDULA:", person["Cedula"])
-    return check_continue()
-
-
-def contacts(new_contact):
-    Contacts.append(new_contact)
-    print("\n\U00002714El contacto con Nombre <<" + new_contact['Nombre']+">> se guardo Exitosamente\U00002714\n\n")
-
-
-def add_contact():
-    print("\n\t\t\U0000260E\U0000260EAGREGA UN CONTACTO\U0000260E\U0000260E")
-    new_contact = {
-        'Nombre': input("Nombre:").lower(),
-        'Apellido': input("Apellido:").lower(),
-        'Telefono': 0,
-        'Cedula': 0
-    }
-    new_contact['Telefono'] = check_tel()
-    new_contact['Cedula'] = check_ced()
-    contacts(new_contact)
-    return check_continue()
-
-
-def check_tel():
-    type_num = True
-    while type_num:
-        user_num = input("N° de telefono: ")
-        try:
-            int(user_num)
-            type_num = False
-            return user_num
-        except ValueError:
-            print("\U000026A0\U000026A0El N° de telefono debe ser un numero entero\U000026A0\U000026A0\n\t\t\U0001F449Intente de nuevo\U0001F448")
-            type_num
-
-
-def check_ced():
-    type_ced = True
-    while type_ced:
-        user_ced = input("N° de Cedula: ")
-        try:
-            int(user_ced)
-            type_ced = False
-            return user_ced
-        except ValueError:
-            print("\U000026A0\U000026A0El N° de Cedula debe ser un numero entero\U000026A0\U000026A0\n\t\t\U0001F449Intente de nuevo\U0001F448")
-            type_ced
+    return funciones_generales.check_continue()
 
 
 def look_for_contact():
-    print("\n\t\t\t\U0001F50E\U0001F50E\U0001F50E BUSCAR CONTACTO \U0001F50D\U0001F50D\U0001F50D")
+    print("\n\t\t\U0001F50E\U0001F50E\U0001F50E BUSCAR CONTACTO \U0001F50D\U0001F50D\U0001F50D")
     if not Contacts:
         print("\t\U0000274C\U0000274C\U0000274CNo existe ningun contacto\U0000274C\U0000274C\U0000274C\n\n")
-        return check_continue()
+        return funciones_generales.check_continue()
     else:
         print("Seleccione el filtro con el cual desea realizar la busqueda:\n1.Nombre\n2.Apellido\n")
         user_filter = ""
@@ -119,9 +58,9 @@ def look_for_contact():
                 "1": look_for_with_name,
                 "2": look_for_with_last_name,
             }
-            search = filter.get(user_filter, error)
+            search = filter.get(user_filter, funciones_generales.error)
             search()
-            return check_continue()
+            return funciones_generales.check_continue()
 
 
 def look_for_with_name():
@@ -160,9 +99,34 @@ def look_for_with_last_name():
         print(f"El contacto con APELLIDO {searched_last_name} NO existe")
 
 
-def exit():
-    print("\t\t\U0001F44B\U0001F44B\U0001F44B\U0001F44BHASTA LUEGO\U0001F44B\U0001F44B\U0001F44B\U0001F44B")
-    return False
+def add_contact():
+    print("\n\t\t\U0000260E\U0000260EAGREGA UN CONTACTO\U0000260E\U0000260E")
+    new_contact = {
+       'Nombre': input("Nombre:").lower(),
+       'Apellido': input("Apellido:").lower(),
+       'Telefono': funciones_generales.check_tel(),
+       'Cedula': check_ced()
+    }
+    contacts(new_contact)
+    return funciones_generales.check_continue()
+
+
+def contacts(new_contact):
+    Contacts.append(new_contact)
+    print("\n\U00002714El contacto con Nombre <<" + new_contact['Nombre']+">> se guardo Exitosamente\U00002714\n\n")
+
+
+def check_ced():
+    type_ced = True
+    while type_ced:
+        user_ced = input("N° de Cedula: ")
+        try:
+            int(user_ced)
+            type_ced = False
+            return user_ced
+        except ValueError:
+            print("\U000026A0\U000026A0El N° de Cedula debe ser un numero entero\U000026A0\U000026A0\n\t\t\U0001F449Intente de nuevo\U0001F448")
+            type_ced
 
 
 view_menu()
